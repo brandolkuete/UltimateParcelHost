ecdd03f3aef921c30ecc13cb17fc71066bf8ca74
brandolkuete <brandolkuete95@gmail.com>
2021-10-29 10:08:25 +0100

    initial version

---
+++ src/main/java/com/taim/conduire/UltimateparcelhostApplication.java
@@ -0,0 +1,13 @@
+package com.taim.conduire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateparcelhostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateparcelhostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taim/conduire/ultimateparcelhost/UltimateparcelhostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taim.conduire.ultimateparcelhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateparcelhostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
